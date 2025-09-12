ed31bc3e2665b0c5621a73485f39f0d62e7027f0
MohammedHasan-004 <mohammedmetani17@gmail.com>
2025-09-12 16:55:53 +0300

    my project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.example.observer.User;
+import org.example.subject.Channel;
+
 public class Main {
     public static void main(String[] args) {
         // TODO:
@@ -7,5 +10,20 @@ public class Main {
         // 2. Create some Users
         // 3. Subscribe Users to Channels
         // 4. Upload videos and see notifications propagate
+        ChannelService svc = new ChannelService();
+        Channel c1 =new Channel("TechWorld");
+        svc.createNewChannel(c1);
+        Channel c2 =new Channel("FoodiesUnite");
+        svc.createNewChannel(c2);
+        User alice = new User("Alice");
+        User bob   = new User("Bob");
+        User charlie = new User("Charlie");
+        svc.subscribeToChannel(c1,alice);
+        svc.subscribeToChannel(c1, bob);
+        svc.subscribeToChannel(c2, alice);
+        svc.subscribeToChannel(c2, charlie);
+        c1.uploadVideo("Observer Pattern Explained");
+        c2.uploadVideo("Best Pasta Recipe");
+
     }
 }
